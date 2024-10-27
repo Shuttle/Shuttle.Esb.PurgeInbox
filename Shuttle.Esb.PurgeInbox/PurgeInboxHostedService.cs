26996d28c3b7d9c820090efeb364d606b5736d89
Eben <me@ebenroux.co.za>
2024-10-27 14:09:44 +0200

    Implemented unified / async-only / nullable.

--- Shuttle.Esb.PurgeInbox/PurgeInboxHostedService.cs
+++ Shuttle.Esb.PurgeInbox/PurgeInboxHostedService.cs
@@ -5,42 +5,41 @@ using Microsoft.Extensions.Hosting;
 using Shuttle.Core.Contract;
 using Shuttle.Core.Pipelines;
 
-namespace Shuttle.Esb.PurgeInbox
+namespace Shuttle.Esb.PurgeInbox;
+
+public class PurgeInboxHostedService : IHostedService
 {
-    public class PurgeInboxHostedService : IHostedService
+    private readonly IPipelineFactory _pipelineFactory;
+    private readonly PurgeInboxObserver _purgeInboxObserver;
+    private readonly Type _startupPipelineType = typeof(StartupPipeline);
+
+    public PurgeInboxHostedService(IPipelineFactory pipelineFactory, PurgeInboxObserver purgeInboxObserver)
     {
-        private readonly IPipelineFactory _pipelineFactory;
-        private readonly PurgeInboxObserver _purgeInboxObserver;
-        private readonly Type _startupPipelineType = typeof (StartupPipeline);
+        _pipelineFactory = Guard.AgainstNull(pipelineFactory);
+        _purgeInboxObserver = Guard.AgainstNull(purgeInboxObserver);
 
-        public PurgeInboxHostedService(IPipelineFactory pipelineFactory, PurgeInboxObserver purgeInboxObserver)
+        _pipelineFactory.PipelineCreated += OnPipelineCreated;
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        await Task.CompletedTask;
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        _pipelineFactory.PipelineCreated -= OnPipelineCreated;
+
+        await Task.CompletedTask;
+    }
+
+    private void OnPipelineCreated(object? sender, PipelineEventArgs e)
+    {
+        if (e.Pipeline.GetType() != _startupPipelineType)
         {
-            _pipelineFactory = Guard.AgainstNull(pipelineFactory, nameof(pipelineFactory));
-            _purgeInboxObserver = Guard.AgainstNull(purgeInboxObserver, nameof(purgeInboxObserver));
-
-            _pipelineFactory.PipelineCreated += OnPipelineCreated;
+            return;
         }
 
-        private void OnPipelineCreated(object sender, PipelineEventArgs e)
-        {
-            if (e.Pipeline.GetType() != _startupPipelineType)
-            {
-                return;
-            }
-
-            e.Pipeline.RegisterObserver(_purgeInboxObserver);
-        }
-
-        public async Task StartAsync(CancellationToken cancellationToken)
-        {
-            await Task.CompletedTask;
-        }
-
-        public async Task StopAsync(CancellationToken cancellationToken)
-        {
-            _pipelineFactory.PipelineCreated -= OnPipelineCreated;
-
-            await Task.CompletedTask;
-        }
+        e.Pipeline.RegisterObserver(_purgeInboxObserver);
     }
 }
